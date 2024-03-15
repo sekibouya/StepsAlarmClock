3d1459d16ee709a471b17bcd17acebf771ddb9c7
sekibouya <yu0629shi@gmail.com>
2024-03-15 12:40:01 +0900

    Initial Commit

---
+++ AlarmClock/AlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockApp.swift
+//  AlarmClock
+//
+//  Created by 関優志 on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
